c774c168a1ec8a69bcce55f56ad95411b72b8855
Daniel Sant'Anna <danielcarlossantanna@gmail.com>
2019-12-20 16:17:29 -0300

    WIP - Jest test

--- src/components/Starships/index.js
+++ src/components/Starships/index.js
@@ -3,10 +3,11 @@ import React from 'react';
 import { Modal, Carousel, ErrorMessage } from '@Uikit';
 import Starship from './Starship';
 
+const errorMessage = 'This character does not have a starship';
+
 const Starships = ({ show, onClose, links }) => {
   if (!show) return null;
-  if (!links.length)
-    return <ErrorMessage onClose={onClose} message="This character does not have a starship" />;
+  if (!links.length) return <ErrorMessage onClose={onClose} message={errorMessage} />;
 
   return (
     <Modal
--- src/components/Uikit/Carousel/index.js
+++ src/components/Uikit/Carousel/index.js
@@ -2,17 +2,16 @@ import React from 'react';
 import NukaCarousel from 'nuka-carousel';
 
 import { ImgIcon } from '@Uikit';
-import leftSrc from '@/assets/icons/left-chevron.svg';
-import rightSrc from '@/assets/icons/right-chevron.svg';
+import LeftSrc from '@/assets/icons/leftChevron.svg';
+import RightSrc from '@/assets/icons/rightChevron.svg';
 
 const ImageControl = props => <ImgIcon style={{ cursor: 'pointer' }} {...props} />;
-
 const LeftControl = ({ previousSlide, ...others }) => (
-  <ImageControl src={leftSrc} onClick={previousSlide} {...others} />
+  <ImageControl src={LeftSrc} onClick={previousSlide} {...others} />
 );
 
 const RightControl = ({ nextSlide, ...others }) => (
-  <ImageControl src={rightSrc} onClick={nextSlide} {...others} />
+  <ImageControl src={RightSrc} onClick={nextSlide} {...others} />
 );
 
 const Carousel = props => (
